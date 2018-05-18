aa284df62cf89980e16e0ef0d6e6f293cdd871d5
Alex Kim <Alex.Kim@lung.org>
2018-05-18 12:02:08 -0500

    wtf atom

--- animation.js
+++ animation.js
@@ -7,18 +7,48 @@ $(this).next().slideToggle('fast');
 $(this).toggleClass('selected');
 $(this).find('span').toggleClass('flip');
 
-
-if (!this.classList.contains("selected") {
-  this.nextElementSibling.style.height = "inherit"; //do this without jQuery
-  this.classList.add("selected");
-} else{
-  this.nextElementSibling.style.height = "0";
+if (!this.classList.contains('selected')) {
+  this.nextElementSibling.style.height = 'inherit';
+  this.nextElementSibling.style.display = 'block';
+  this.classList.add('selected');
+  this.find('span').classList.add('flip');
+} else {
+  this.nextElementSibling.style.display = 'none';
+  this.nextElementSibling.style.height = '0';
+  this.classList.remove('selected');
+  this.find('span').classList.remove('flip');
 }
 
-
-
-if (event.keyCode == (13 || 32)) {
-  $(this).next().slideToggle('fast');
-  $(this).toggleClass('selected');
-  $(this).find('span').toggleClass('flip')
+function(e){
+  if (!e.currentTarget.classList.contains('selected')) {
+    e.currentTarget.nextElementSibling.style.height = 'inherit';
+    e.currentTarget.nextElementSibling.style.display = 'block';
+    e.currentTarget.classList.add('selected');
+    e.currentTarget.querySelector('span').classList.add('flip');
+  } else {
+    e.currentTarget.nextElementSibling.style.display = 'none';
+    e.currentTarget.nextElementSibling.style.height = '0';
+    e.currentTarget.classList.remove('selected');
+    e.currentTarget.querySelector('span').classList.remove('flip');
+  }
 }
+
+function(e){
+  if (e.keyCode == (13 || 32)) {
+    if (!e.currentTarget.classList.contains('selected')) {
+      e.currentTarget.nextElementSibling.style.height = 'inherit';
+      e.currentTarget.nextElementSibling.style.display = 'block';
+      e.currentTarget.classList.add('selected');
+      e.currentTarget.querySelector('span').classList.add('flip');
+    } else {
+      e.currentTarget.nextElementSibling.style.display = 'none';
+      e.currentTarget.nextElementSibling.style.height = '0';
+      e.currentTarget.classList.remove('selected');
+      e.currentTarget.querySelector('span').classList.remove('flip');
+    }
+  }
+}
+
+function(e){if(!e.currentTarget.classList.contains('selected')){e.currentTarget.nextElementSibling.style.height='inherit';e.currentTarget.nextElementSibling.style.display='block';e.currentTarget.classList.add('selected');e.currentTarget.querySelector('span').classList.add('flip')}else{e.currentTarget.nextElementSibling.style.display='none';e.currentTarget.nextElementSibling.style.height='0';e.currentTarget.classList.remove('selected');e.currentTarget.querySelector('span').classList.remove('flip')}}
+
+function(e){if(e.keyCode==(13||32)){if(!e.currentTarget.classList.contains('selected')){e.currentTarget.nextElementSibling.style.height='inherit';e.currentTarget.nextElementSibling.style.display='block';e.currentTarget.classList.add('selected');e.currentTarget.querySelector('span').classList.add('flip')}else{e.currentTarget.nextElementSibling.style.display='none';e.currentTarget.nextElementSibling.style.height='0';e.currentTarget.classList.remove('selected');e.currentTarget.querySelector('span').classList.remove('flip')}}}
