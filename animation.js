979e49baa52c533c1aa047c1bfda7b51a93f0f58
Alex Kim <Alex.Kim@lung.org>
2018-05-18 17:10:30 -0500

    these freaking animations

--- animation.js
+++ animation.js
@@ -7,50 +7,6 @@ $(this).next().slideToggle('fast');
 $(this).toggleClass('selected');
 $(this).find('span').toggleClass('flip');
 
-if (!this.classList.contains('selected')) {
-  this.nextElementSibling.style.height = 'inherit';
-  this.nextElementSibling.style.display = 'block';
-  this.classList.add('selected');
-  this.find('span').classList.add('flip');
-} else {
-  this.nextElementSibling.style.display = 'none';
-  this.nextElementSibling.style.height = '0';
-  this.classList.remove('selected');
-  this.find('span').classList.remove('flip');
-}
-
-function(e) {
-  if (!e.currentTarget.classList.contains('selected')) {
-    e.currentTarget.nextElementSibling.style.height = 'inherit';
-    e.currentTarget.nextElementSibling.style.display = 'block';
-    e.currentTarget.classList.add('selected');
-    e.currentTarget.querySelector('span').classList.add('flip');
-  } else {
-    e.currentTarget.nextElementSibling.style.display = 'none';
-    e.currentTarget.nextElementSibling.style.height = '0';
-    e.currentTarget.classList.remove('selected');
-    e.currentTarget.querySelector('span').classList.remove('flip');
-  }
-}
-
-function(e) {
-  if (e.keyCode == (13 || 32)) {
-    if (!e.currentTarget.classList.contains('selected')) {
-      e.currentTarget.nextElementSibling.style.height = 'inherit';
-      e.currentTarget.nextElementSibling.style.display = 'block';
-      e.currentTarget.classList.add('selected');
-      e.currentTarget.querySelector('span').classList.add('flip');
-    } else {
-      e.currentTarget.nextElementSibling.style.display = 'none';
-      e.currentTarget.nextElementSibling.style.height = '0';
-      e.currentTarget.classList.remove('selected');
-      e.currentTarget.querySelector('span').classList.remove('flip');
-    }
-  }
-}
-
-// END WORKING CODE --------------------------
-// START EDITED AND/OR MINIFIED SECTION ---------------------
 
 (e) => {
   if (!e.currentTarget.classList.contains('selected')) {
@@ -66,10 +22,8 @@ function(e) {
   }
 }
 
-(e)=>{if(!e.currentTarget.classList.contains('selected')){e.currentTarget.nextElementSibling.style.height='inherit';e.currentTarget.nextElementSibling.style.display='block';e.currentTarget.classList.add('selected');e.currentTarget.querySelector('span').classList.add('flip');}else{e.currentTarget.nextElementSibling.style.display='none';e.currentTarget.nextElementSibling.style.height='0';e.currentTarget.classList.remove('selected');e.currentTarget.querySelector('span').classList.remove('flip');}}
-
 (e) => {
-  if (e.keyCode == (13 || 32)) {
+  if (e.key=='Enter') {
     if (!e.currentTarget.classList.contains('selected')) {
       e.currentTarget.nextElementSibling.style.height = 'inherit';
       e.currentTarget.nextElementSibling.style.display = 'block';
@@ -84,4 +38,78 @@ function(e) {
   }
 }
 
-(e)=>{if(e.keyCode==(13||32)){if(!e.currentTarget.classList.contains('selected')){e.currentTarget.nextElementSibling.style.height='inherit';e.currentTarget.nextElementSibling.style.display='block';e.currentTarget.classList.add('selected');e.currentTarget.querySelector('span').classList.add('flip');}else{e.currentTarget.nextElementSibling.style.display='none';e.currentTarget.nextElementSibling.style.height='0';e.currentTarget.classList.remove('selected');e.currentTarget.querySelector('span').classList.remove('flip');}}}
+
+// END WORKING CODE --------------------------
+// START EDITED AND/OR MINIFIED SECTION ---------------------
+
+(e) => {
+  if (!e.currentTarget.classList.contains('selected')) {
+    e.currentTarget.nextElementSibling.classList.add('show');
+    e.currentTarget.nextElementSibling.classList.remove('hide');
+    e.currentTarget.classList.add('selected');
+    e.currentTarget.querySelector('span').classList.add('flip');
+  } else {
+    e.currentTarget.nextElementSibling.classList.remove('show');
+    e.currentTarget.nextElementSibling.classList.add('hide');
+    e.currentTarget.classList.remove('selected');
+    e.currentTarget.querySelector('span').classList.remove('flip');
+  }
+}
+
+(e)=>{if(!e.currentTarget.classList.contains('selected')){e.currentTarget.nextElementSibling.classList.add('show');e.currentTarget.nextElementSibling.classList.remove('hide');e.currentTarget.classList.add('selected');e.currentTarget.querySelector('span').classList.add('flip')}else{e.currentTarget.nextElementSibling.classList.remove('show');e.currentTarget.nextElementSibling.classList.add('hide');e.currentTarget.classList.remove('selected');e.currentTarget.querySelector('span').classList.remove('flip')}}
+
+
+
+(e) => {
+  if (e.key=='Enter') {
+    if (!e.currentTarget.classList.contains('selected')) {
+      e.currentTarget.nextElementSibling.classList.add('show');
+      e.currentTarget.nextElementSibling.classList.remove('hide');
+      e.currentTarget.classList.add('selected');
+      e.currentTarget.querySelector('span').classList.add('flip');
+    } else {
+      e.currentTarget.nextElementSibling.classList.remove('show');
+      e.currentTarget.nextElementSibling.classList.add('hide');
+      e.currentTarget.classList.remove('selected');
+      e.currentTarget.querySelector('span').classList.remove('flip');
+    }
+  }
+}
+
+(e)=>{if(e.key=='Enter'){if(!e.currentTarget.classList.contains('selected')){e.currentTarget.nextElementSibling.classList.add('show');e.currentTarget.nextElementSibling.classList.remove('hide');e.currentTarget.classList.add('selected');e.currentTarget.querySelector('span').classList.add('flip')}else{e.currentTarget.nextElementSibling.classList.remove('show');e.currentTarget.nextElementSibling.classList.add('hide');e.currentTarget.classList.remove('selected');e.currentTarget.querySelector('span').classList.remove('flip')}}}
+
+// JUST ONE
+
+(e) => {
+  if (!e.currentTarget.classList.contains('selected')) {
+    e.currentTarget.nextElementSibling.classList.add('showA');
+    e.currentTarget.nextElementSibling.classList.add('showB');
+    e.currentTarget.classList.add('selected');
+    e.currentTarget.querySelector('span').classList.add('flip');
+  } else {
+    e.currentTarget.nextElementSibling.classList.remove('showA');
+    e.currentTarget.nextElementSibling.classList.remove('showB');
+    e.currentTarget.classList.remove('selected');
+    e.currentTarget.querySelector('span').classList.remove('flip');
+  }
+}
+
+(e)=>{if(!e.currentTarget.classList.contains('selected')){e.currentTarget.nextElementSibling.classList.add('showA');e.currentTarget.nextElementSibling.classList.add('showB');e.currentTarget.classList.add('selected');e.currentTarget.querySelector('span').classList.add('flip')}else{e.currentTarget.nextElementSibling.classList.remove('showA');e.currentTarget.nextElementSibling.classList.remove('showB');e.currentTarget.classList.remove('selected');e.currentTarget.querySelector('span').classList.remove('flip')}}
+
+(e) => {
+  if (e.key=='Enter') {
+    if (!e.currentTarget.classList.contains('selected')) {
+      e.currentTarget.nextElementSibling.classList.add('showA');
+      e.currentTarget.nextElementSibling.classList.add('showB');
+      e.currentTarget.classList.add('selected');
+      e.currentTarget.querySelector('span').classList.add('flip');
+    } else {
+      e.currentTarget.nextElementSibling.classList.remove('showA');
+      e.currentTarget.nextElementSibling.classList.remove('showB');
+      e.currentTarget.classList.remove('selected');
+      e.currentTarget.querySelector('span').classList.remove('flip');
+    }
+  }
+}
+
+(e)=>{if(e.key=='Enter'){if(!e.currentTarget.classList.contains('selected')){e.currentTarget.nextElementSibling.classList.add('showA');e.currentTarget.nextElementSibling.classList.add('showB');e.currentTarget.classList.add('selected');e.currentTarget.querySelector('span').classList.add('flip')}else{e.currentTarget.nextElementSibling.classList.remove('showA');e.currentTarget.nextElementSibling.classList.remove('showB');e.currentTarget.classList.remove('selected');e.currentTarget.querySelector('span').classList.remove('flip')}}}
